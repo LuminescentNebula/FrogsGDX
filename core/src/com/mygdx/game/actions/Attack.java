b4cf4df2e5f1633bbdec0a0042bf7a53473a1751
LuminescentNebula <dima.lippo@bk.ru>
2023-10-23 06:51:19 +0300

    stopOnFirstCollision flag fixed. It used to mean first by order in array, now it by distance from selected

--- core/src/com/mygdx/game/actions/Attack.java
+++ core/src/com/mygdx/game/actions/Attack.java
@@ -17,15 +17,7 @@ import com.mygdx.game.interfaces.Health;
 import java.util.*;
 import java.util.stream.Stream;
 
-//TODO:Нужно рисовать зону поражения в act
 //TODO:Настройки урона
-//TODO:фабрика нужна
-//        CONE,
-//        SINGLE_TARGET, CIRCLE
-//        SHOT,
-//        CATAPULT,
-//        CHAIN -- режим
-//        RANGE
 
 public class Attack {
     private boolean isSelected=false;
@@ -33,12 +25,7 @@ public class Attack {
 
     private ArrayList<Type> types;
     private Flag flags;
-//    private float radius; //chainradius=radius
-//    private float minLength;
-//    private float maxLength;
-//    private int damage = 10;
-    //TODO: Переместить в Type
-    private Set<Health> targets = new LinkedHashSet<>();
+
     boolean chainChecking = false; //Temporal while chain checking
 
     public static final Color areaColor=new Color(1,1,0,0.3f);
@@ -51,10 +38,12 @@ public class Attack {
     }
 
     public void check(ShapeRenderer shapeRenderer, Attackable master, Vector2 cursor,MainPool mainPool){
+        shapeRenderer.setColor(new Color(areaColor));
+        flushTargets();
         for (Type type : types ){
             type.performPre(master.getCenter(),cursor,mainPool);
             draw(type::draw,shapeRenderer,master.getCenter(),cursor, type.getMinLength(),type.getMaxLength(),type.getRadius());
-            act(type::check,mainPool,shapeRenderer,master,cursor,type.getRadius());
+            act(type,mainPool,shapeRenderer,master,cursor,type.getRadius());
         }
     }
 
@@ -70,24 +59,18 @@ public class Attack {
         Gdx.gl.glBlendFunc(GL30.GL_SRC_ALPHA, GL30.GL_ONE_MINUS_SRC_ALPHA);
 
         drawInterface.draw(shapeRenderer,master,cursor,minLength,maxLength,radius);
-        flushTargets();
     }
 
     public void flushTargets() {
-        System.out.print("{");
-        for (Health health:targets) {
-            System.out.print(health.getId()+" ");
-            health.setTargeted(false);
-        }
-        System.out.print("}\n");
-        targets = new HashSet<>();
+        types.forEach(Type::flushTargets);
     }
 
-    public void act(ActInterface actInterface, MainPool mainPool, ShapeRenderer shapeRenderer, Attackable master, Vector2 cursor, float radius) {
-        shapeRenderer.setColor(new Color(areaColor));
-
+    public void act(Type type, MainPool mainPool, ShapeRenderer shapeRenderer, Attackable master, Vector2 cursor, float radius) {
 
         Stream<Health> stream = mainPool.getHealths().stream();
+        stream =stream.sorted(Comparator.comparing(health -> {
+            return health.getCenter().dst(master.getCenter());
+        }));
         if (flags.is(Flag.checkNotMaster)) {
             stream = (stream.filter(other -> master.getId() != other.getId()));
         }
@@ -96,7 +79,7 @@ public class Attack {
             stream = stream.filter(other -> Type.chainCheck(other, shapeRenderer,
                     new Circle(cursor, radius)));
         } else { //Проверяем коллизию для всех остальных
-            stream = stream.filter(other -> actInterface.check(other, master.getCenter(), cursor,
+            stream = stream.filter(other -> type.check(other, master.getCenter(), cursor,
                     new Circle(cursor, radius)));
         }
 
@@ -104,31 +87,19 @@ public class Attack {
             flags.del(Flag.stopOnFirstCollision);
             chainChecking=true;
             stream.findFirst().ifPresent(other -> { //Если есть chainDamage, то вызывается act, но с удаленным stopOnFirst и отметкой chainChecking, что идет проверка цепи
-                if (isAdd(other) && flags.is(Flag.chainDamage)) act(actInterface, mainPool, shapeRenderer, master, other.getCenter(), radius);
+                if (type.isAdd(other) && flags.is(Flag.chainDamage)) act(type, mainPool, shapeRenderer, master, other.getCenter(), radius);
             }); //Цепочка тоже начинается только от первой цели
             chainChecking=false;
             flags.add(Flag.stopOnFirstCollision);
         } else {
             stream.forEach(other -> { //Цепочка начинается от каждой цели
-                if (isAdd(other) && flags.is(Flag.chainDamage)) {
-                    act(actInterface,mainPool, shapeRenderer, master, other.getCenter(), radius);
+                if (type.isAdd(other) && flags.is(Flag.chainDamage)) {
+                    act(type,mainPool, shapeRenderer, master, other.getCenter(), radius);
                 };
             });
         }
     }
-    /**
-     * Trying to add given Health object should and return success of operation.
-     *
-     * @param  other  the Health object to be added
-     * @return        true if the Health object was successfully added, otherwise false
-     */
-    private boolean isAdd(Health other) {
-        if (targets.add(other)){
-            other.setTargeted(true);
-            return true;
-        }
-        return false;
-    }
+
     public void setSelected(boolean selected) {
         isSelected = selected;
     }
